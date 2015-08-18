5463d6aa7066af19e3210cadf26973ec9803b5bb
Lanford Gabriel Murillo <holtlanf999@gmail.com>
2015-08-18 17:00:53 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var loadEmbedPage = ( function( window, undefined ){
+	
+	// variables
+	
+	// variables
+
+	// methods
+
+	// methods
+
+
+	// return methods
+
+	// return methods
+
+
+})
+
+// call methods
+
+// call methods
