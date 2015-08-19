287aad2030113d875ad3d9f74ff684f52cb4342e
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-08-19 02:31:35 -0600

    agregada fucionabilidad de JS para correr demo onclick

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-var loadEmbedPage = ( function( window, undefined ){
+var loadEmbedContent = ( function( window, undefined ){
 	
 	// variables
-	
+	var demo1Src = 'src="http://e.gsrca.de/128666?s=560&mode=embed"';
+	var demo2Src = 'src="http://thelastactivist.s3.amazonaws.com/index.html"';
+	var demo3Src = 'src="https://dl.dropboxusercontent.com/u/18417484/TrashFlood.html"';
+	var demo4Src = 'src="https://dl.dropboxusercontent.com/u/3223958/Owak%20and%20the%20Wooden%20Tower.html"';
 	// variables
 
 	// methods
+	function demoLoad( id ){
+		var playButton = document.getElementById( 'demoLoad' + id );
+		var demoLoadId = document.getElementById( 'demo' + id );
 
+		console.log( playButton );
+		console.log( demoLoadId );
+
+		playButton.onClick( function( id ){
+			demoLoadId.setAttribute( 'src', 'demo' + id + 'Src');
+		});
+	}
 	// methods
 
-
 	// return methods
-
+	return {
+		initOnClick : demoLoad
+	}
 	// return methods
+} )( window );
 
-
-})
+loadEmbedContent.demoLoad( id );
 
 // call methods
 
